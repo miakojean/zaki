b081ea2c9cfd0d6bf1885d58f13c3925760b6fbe
miakojean <miakojeanyves@gmail.com>
2024-12-19 15:02:23 +0000

    models first part

--- zakiback/zakiapi/models.py
+++ zakiback/zakiapi/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Orders"
+
+    def __str__(self):
+        return f"Order by {self.user} for {self.product.name}"
